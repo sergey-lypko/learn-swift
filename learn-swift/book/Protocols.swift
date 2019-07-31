d3828dbb9d432c866042be20c714753bb5f448d0
Sergey Lypko <lypko.sg@gmail.com>
2019-07-31 16:58:48 +0300

    Update Protocols.swift

--- learn-swift/book/Protocols.swift
+++ learn-swift/book/Protocols.swift
@@ -1,1 +1,35 @@
+// протоколы являются "чертежами" для структур данных в swift
+// они описывают (без каких-либо реализаций) требования, которые 
+// та или иная структура данных должна удовлетворять
 
+
+// *  *  *  Property Requirements  *  *  *
+// *  *  *  *  *  *  *  *  *  *  *  *  *  *
+
+protocol FullyNamed {
+    // read-only property
+    var fullName: String { get }
+}
+
+class Starship: FullyNamed {
+    var prefix: String?
+    var name: String
+    init(name: String, prefix: String? = nil) {
+        self.name = name
+        self.prefix = prefix
+    }
+  
+    // computed read-only property
+    var fullName: String {
+        return (prefix != nil ? prefix! + " " : "") + name
+    }
+}
+var ncc1701 = Starship(name: "Enterprise", prefix: "USS")
+
+
+
+// *  *  *  Method Requirements  *  *  *  *
+// *  *  *  *  *  *  *  *  *  *  *  *  *  *
+
+// no defaul values
+// always with static keywoard
